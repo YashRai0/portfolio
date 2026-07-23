48afa1d1173a6dd36aa4e239c44064a02885d1d3
YashRai0 <yashrai6635@gmail.com>
2026-07-23 23:20:43 +0530

    feat: integrate nitro plugin to vite.config.ts for serverless SSR Vercel output

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tanstackStart(), devtools(), tailwindcss()],
+  plugins: [tanstackStart(), nitro(), devtools(), tailwindcss()],
 })
 
 export default config
